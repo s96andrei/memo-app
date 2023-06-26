579001a695bab642e683bff638f9a044d5da0bf9
Andrei Stefanica <102757943+s96andrei@users.noreply.github.com>
2023-06-26 13:49:08 +0300

    Task Add Angular
    Remove tasks by ID
    Save to Local Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit,  ViewChild, NgModule,} from '@angular/core';
+import { UUID } from 'uuid-generator-ts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'memo-app';
+
+  newValue: string = '';
+  // memoList: Array<string> = [];
+
+  itemInterface: {
+    id: object;
+    value: string;
+  }[] = [];
+
+  @ViewChild('newInput') newInput!: ElementRef;
+
+  ngOnInit(): void {
+  
+    const itemInterface = localStorage.getItem('memoList');
+    if (itemInterface) {
+      this.itemInterface = JSON.parse(itemInterface);
+    }
+  }
+
+  changeNewValue(event: string) {
+    console.log(event);
+    this.newValue = event;
+  }
+
+  saveNewValue() {
+
+    if (this.newValue.length < 1) {
+      alert('Enter a value');
+      return;
+    };
+
+
+    const newItem = {
+      id: new UUID(), 
+      value: this.newValue,
+    };
+
+    this.newValue = '';
+
+    this.itemInterface.push(newItem);
+
+    if (this.newInput) {
+      this.newInput.nativeElement.value = '';
+    }
+
+    localStorage.setItem('memoList', JSON.stringify(this.itemInterface));
+
+    console.log(this.itemInterface);
+  }
+
+  removeItem(id: object) {
+
+    // Filter the itemInterface array to exclude the item with the specified ID
+    const updatedItemInterface = this.itemInterface.filter(
+      (item) => item.id !== id
+    );
+    console.log(updatedItemInterface);
+
+    // Update the itemInterface array with the filtered array
+    this.itemInterface = updatedItemInterface;
+    console.log(this.itemInterface);
+  }
+
+  deleteAll() {
+    this.itemInterface = [];
+    console.log(this.itemInterface);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
